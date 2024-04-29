5b82fd3733b283ceed6a0c8bdedafe2a15c77cff
varun040503 <varunreddy2439@gmail.com>
2024-04-29 12:54:41 +0530

    code

---
+++ src/main/java/com/varun/project/ProjectApplication.java
@@ -0,0 +1,22 @@
+package com.varun.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProjectApplication {
+
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Adda lab fat varun-20MIC0151";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}
